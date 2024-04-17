d3060ee1b726748c736254252d0c0794b8175f2e
Rahulpoppad <158065503+Rahulpoppad@users.noreply.github.com>
2024-04-17 21:23:51 +0100

    Update settings.py

--- emspr/settings.py
+++ emspr/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'emspr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,14 +121,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'eventapp/static'),
+    os.path.join(BASE_DIR, 'eventapp', 'static'),
+    os.path.join(BASE_DIR, 'userapp', 'static'),
 ]
 
 
 # Media Files
 
-MEDIA_ROOT=BASE_DIR/'pic'
-MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pic')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

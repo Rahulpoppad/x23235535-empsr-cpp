999df4272b98fe1529fff3a522678988658c0386
Rahulpoppad <158065503+Rahulpoppad@users.noreply.github.com>
2024-04-17 20:02:50 +0100

    Update settings.py

--- emspr/settings.py
+++ emspr/settings.py
@@ -119,10 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Media Files
+
 MEDIA_ROOT=BASE_DIR/'pic'
 MEDIA_URL='/media/'
 

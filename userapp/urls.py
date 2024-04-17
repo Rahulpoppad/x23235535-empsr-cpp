1efd1c97f1fee0cc3d3014b4d4f39ae1d83c2f16
Rahulpoppad <158065503+Rahulpoppad@users.noreply.github.com>
2024-04-17 22:29:05 +0100

    Update urls.py

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,7 +3,7 @@ from userapp import views
 
 urlpatterns = [
     path('', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('password_reset/', include('django.contrib.auth.urls')),  # Include Django's password reset URLs
     # Add other custom URL patterns as needed
